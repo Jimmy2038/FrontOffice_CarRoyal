915b7b2474be5f9d5f9b54b7d82f6cc3a1dea955
Jimmy2038 <119540791+Jimmy2038@users.noreply.github.com>
2024-02-10 12:26:29 +0100

    Update _headerIndex.js

--- src/components/_headerIndex.js
+++ src/components/_headerIndex.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Topbar from "./_topbar";
 import NavbarIndex from "./_navbarIndex";
-import SearchIndex from "./_searchIndex";
+// import SearchIndex from "./_searchIndex";
 
 const HeaderIndex = () => {
   return (
     <>
       <Topbar />
       <NavbarIndex />
-      <SearchIndex />
+      // <SearchIndex />
     </>
   );
 };
