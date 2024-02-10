bef07ad773e4e592ebf614e2c60f60dfa608c4a3
Jimmy2038 <119540791+Jimmy2038@users.noreply.github.com>
2024-02-10 12:50:39 +0100

    Update _detail.js

--- src/pages/_detail.js
+++ src/pages/_detail.js
@@ -59,18 +59,7 @@ const Detail = () => {
   return (
     <>
       <HeaderInfo />
-      <div className="container-fluid page-header">
-        <h1 className="display-3 text-uppercase text-white mb-3">Car Detail</h1>
-        <div className="d-inline-flex text-white">
-          <h6 className="text-uppercase m-0">
-            <a className="text-white" href="/">
-              Home
-            </a>
-          </h6>
-          <h6 className="text-body m-0 px-3">/</h6>
-          <h6 className="text-uppercase text-body m-0">Car Detail</h6>
-        </div>
-      </div>
+  
       {error}
       <div className="container-fluid pt-5">
         <div className="container pt-5 pb-3">
